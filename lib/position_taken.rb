330e79f106125700735be6f823fcfe4b7f7a87ab
Roshaan Singh <swaggsikh@gmail.com>
2021-03-18 18:59:18 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -3,7 +3,7 @@
 def position_taken?(board, index)
   if board[index] == " " || "" || NIL
     FALSE
-  end
-  when board[index] == "X" || "O"
+  elsif board[index] == "X" || "O"
     TRUE
+  end
 end
