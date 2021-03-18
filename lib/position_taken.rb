98344712e9c86bc0d05423e56ff1b04f57e50f01
Roshaan Singh <swaggsikh@gmail.com>
2021-03-18 18:47:16 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,10 +1,10 @@
- # code your #position_taken? method here!
+# code your #position_taken? method here!
 
 def position_taken?(board, index)
-  if board[index] != "X" || "O"
+  if board[index] == " " || "" || NIL
     FALSE
-  else 
-    board[index] == "X" || "O"
+  end
+  if [index] == "X" || "O"
     TRUE
   end
 end
